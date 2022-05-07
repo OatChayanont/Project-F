1aad5e51b0b1df6590b064fe04dbafcfb1a055e0
Adamam <88430865+Adamam13@users.noreply.github.com>
2022-05-08 06:46:47 +0700

    add event

--- farmgame.c
+++ farmgame.c
@@ -4,10 +4,15 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <windows.h>
+#include <time.h>
 
 char type[100];
 int day = 1, stamina = 16, money = 800;
 int dc = 0; //discount
+int oc = 0; //overcharge
+int bad_product = 0;
+int shop_close = 0;
+char event[200];
 int error = 0;
 
 struct plant{
@@ -48,11 +53,144 @@ int istype(char check[100]){
 
 }
 
-void loading(char text[100]){
+void loading(char text[100], int num){
     printf("%s", text);
-    for (int i = 0; i <= 2; i++){
+    for (int i = 0; i <= num; i++){
         printf(" .");
         sleep(1);
+    }
+}
+
+void event_generate(){
+    re_random:
+    srand(time(NULL));
+    int random = rand();
+    random %= 10;
+    if (random == 9 && money <= 1000){
+        goto re_random;
+    }
+    int num_farm = 0;
+    for (int i = 1; i <= 9; i++){
+        if (f[i].use){
+            num_farm++;
+        }
+    }
+    if ((random == 0 || random == 1) && num_farm <= 0){
+        goto re_random;
+    }
+    dc = 0;
+    oc = 0;
+    bad_product = 0;
+    shop_close = 0;
+    printf("    Day: %d\n", day);
+    strcpy(event, "-");
+    if (day % 2 == 0){
+        switch (random){
+            case 0:
+                loading("The gods bless your plant.", 5);
+                strcpy(event, "1 mature plant at random");
+
+                while(1){
+                    int fairy = rand();
+                    fairy %= 10;
+                    if (f[fairy+1].use){
+                        strcpy(f[fairy+1].status, "bloom");
+                        f[fairy+1].w = f[fairy+1].mw;
+                        break;
+                        }
+                    }
+                break;
+            case 1:
+                loading("The devil destroys your plant.", 5);
+                strcpy(event, "1 plant will be removed at random.");
+                while(1){
+                    int devil = rand();
+                    devil %= 10;
+                    if (f[devil+1].use){
+                        strcpy(f[devil+1].status, "usual");
+                        f[devil+1].w = 0;
+                        f[devil+1].use = 0;
+                        break;
+                        }
+                    }
+                break;
+            case 2:
+                loading("The government cut the price of vegetables today.", 5);
+                strcpy(event, "The product of your plant decreases by 50%.");
+                bad_product = 1;
+                break;
+            case 3:
+                loading("It's raining today.", 5);
+                strcpy(event, "Your plants are watered.");
+                for (int i = 1; i <= 9; i++){
+                    f[i].w++;
+                }
+                break;
+            case 4:
+                loading("You're feeling sick today.", 5);
+                strcpy(event, "Your stamina decreases to 10.");
+                stamina = 10;
+                break;
+            case 5:
+                loading("A venture capitalist visits your farm today.", 5);
+                strcpy(event, "The product of your plant increases by 25%.");
+                oc = 1;
+                break;
+            case 6:
+                loading("The owner of the shop goes on vacation today.", 5);
+                strcpy(event, "You can't shop at the store.");
+                shop_close = 1;
+                break;
+            case 7:
+                loading("The owner won the lottery today.", 5);
+                strcpy(event, "The price of goods in the shop drops by 20%.");
+                dc = 1;
+                break;
+            case 8:
+                loading("You met a kind man.", 5);
+                strcpy(event, "You get free seeds randomly.");
+                int free_seed = rand();
+                free_seed %= 10;
+                int free_seed_ea = rand();
+                free_seed_ea %= 5;
+                free_seed_ea++;
+                p[free_seed+1].ea += free_seed_ea;
+                printf("\nYou get %s seed %d EA.", p[free_seed+1].name, free_seed_ea);
+                loading(".", 2);
+                break;
+            case 9:
+                loading("It's your bad day today.", 5);
+                int robb = rand();
+                robb %= 5;
+                robb++;
+                robb = money * (robb * 0.1);
+                printf("%d\n", robb);
+                money -= robb;
+                printf("\nYou were robbed of %d.", robb);
+                loading(".", 2);
+                strcpy(event, "You were robbed");
+                break;
+            default:
+                break;
+        }
+    }
+    else if(day == 5 && num_farm >= 1){
+        int insect = rand();
+        insect %= num_farm;
+        insect++;
+        int lucky_plant = rand()%10;
+        int multiplier = rand()%10;
+        int increment = rand()%10;
+        for (int i = 0; i < insect; i++){
+            if(f[lucky_plant].use){
+            f[lucky_plant].insect = 1;
+            strcpy(f[lucky_plant].status, "insect");
+            }
+            else{
+                i--;
+            }
+            lucky_plant = ((lucky_plant*multiplier) + increment) % 9;
+        }
     }
 }
 
@@ -85,7 +223,12 @@ void home(){
             stamina = 16;
             for (int i = 1;i <= 9;i++){
                 f[i].water_yet = 0;
+                if ((strcmp(f[i].status, "usual") == 0) && p[f[i].plant].regrowth && f[i].w >= f[i].mw){
+                    strcpy(f[i].status, "bloom");
+                }
             }
+            system("cls");
+            event_generate();
             system("cls");
             break;
         }
@@ -124,7 +267,7 @@ void shop(){
         printf("| 12. stimulus (stamina +2)                  | %5d |\n", item[12].buy);
         printf("\n");
         printf("===================================== [x] to exit ====\n");
-        printf("event:\n");
+        printf("event: %s\n", event);
         printf("======================================================\n");
         printf("\n");
         printf("++ type a number before order to buy seed or item that you want ++\n");
@@ -144,21 +287,31 @@ void shop(){
         int price = 0;
         if (1 <= id && id <= 12){
             if (id <= 9){
-                price = p[id].buy;
-                price -= (price*(dc/100));
+                if(dc){
+                    price = p[id].buy - (p[id].buy * 0.2);
+                }
+                else{
+                    price = p[id].buy;
+                }
                 if (money >= price){
                     money -= price;
                     p[id].ea++;
+                    printf("    money - %d\n", price);
                     printf("    You by %s seed. Now you have %s seed %d ea.\n", p[id].name, p[id].name, p[id].ea);
                 }
                 else printf("   You don't have enough money\n");
             }
             else {
-                price = item[id].buy;
-                price -= (price*(dc/100));
+                if(dc){
+                    price = item[id].buy - (item[id].buy * 0.2);
+                }
+                else{
+                    price = item[id].buy;
+                }
                 if (money >= price){
                     money -= price;
                     item[id].ea++;
+                    printf("    money - %d\n", price);
                     printf("    You by %s seed. Now you have %s seed %d ea.\n", item[id].name, item[id].name, item[id].ea);
                 }
                 else printf("   You don't have enough money\n");
@@ -167,7 +320,7 @@ void shop(){
         else{
             printf("    Error\n");
         }
-        loading("wait a minute .");
+        loading("wait a minute .", 2);
     }
 }
 
@@ -192,7 +345,7 @@ void farm(){
             printf("\n");
             for (int j = 1; j <= 3; j++){
                 if (f[j+(i*3)].use){
-                printf("  |    %d/%d     |  ", f[j+(i*3)].w, f[j+(i*3)].mw);
+                printf("  |         %d/%d|  ", f[j+(i*3)].w, f[j+(i*3)].mw);
                 }
                 else printf("  |            |  ");
             }
@@ -215,7 +368,7 @@ void farm(){
         }
         printf("\n");
         printf("======================================================\n");
-        printf("event:\n");
+        printf("event: %s\n", event);
         printf("======================================================\n");
         printf("\n");
         printf(" type [P] to plant (-1 stamina per once)\n");
@@ -261,7 +414,12 @@ void farm(){
                 scanf(" %[^\n]", type);
                 int c_num = atoi(type);
                 for (int i = 1; i <= 9; i++){
-                    if ((i == c_num || istype(p[i].name)) && p[i].ea >= 1){
+                    if ((i == c_num || istype(p[i].name))){
+                        if(p[i].ea <= 0){
+                            loading("You don't have seed", 2);
+                            system("cls");
+                            goto start;
+                        }
                         stamina--;
                         p[i].ea--;
                         f[num].use = 1;
@@ -287,12 +445,9 @@ void farm(){
                             }
                         }
                         printf("Stamina -1\n");
-                        loading("Plant");
+                        printf("%s seed -1ea\n", p[f[num].plant].name);
+                        loading("Plant", 2);
                         system("cls");
-                        goto start;
-                    }
-                    else if(p[i].ea <= 0){
-                        printf("You don't have seed\n");
                         goto start;
                     }
                 }
@@ -330,12 +485,12 @@ void farm(){
                 if (f[num].water_yet == 0){
                 f[num].w++;
                 f[num].water_yet = 1;
-                printf("Stamina -1\n");
-                printf("Succeed\n");
                 if (f[num].w >= f[num].mw){
                     strcpy(f[num].status, "bloom");
                 }
-                loading("Water");
+                printf("Stamina -1\n");
+                printf("Succeed\n");
+                loading("Water", 2);
                 }
             }
             else {
@@ -368,11 +523,11 @@ void farm(){
                 f[num].use = 0;
                 printf("Stamina -1\n");
                 printf("Succeed\n");
-                loading("Remove");
+                loading("Remove", 2);
             }
             else if (f[num].use == 0){
                 printf("Don't have plant to remove in this farm\n");
-                loading("Loading");
+                loading("Loading", 2);
             }
             else {
                 printf("Error\n");
@@ -402,25 +557,33 @@ void farm(){
             if (1 <= num && num <= 9 && f[num].use == 1){
                 stamina--;
                 if (strcmp(f[num].status, "bloom") == 0){
-                    if (p[f[num].plant].regrowth){
-                        strcpy(f[num].status, "usual");
-                        money += p[f[num].plant].sale;
+                    if (! p[f[num].plant].regrowth){
+                        f[num].use = 0;
+                    }
+                    strcpy(f[num].status, "usual");
+                    if (bad_product){
+                        money += (p[f[num].plant].sale / 2);
+                    }
+                    else if(oc){
+                        money += (p[f[num].plant].sale + (p[f[num].plant].sale * 0.25));
                     }
                     else{
-                        f[num].use = 0;
-                        strcpy(f[num].status, "usual");
                         money += p[f[num].plant].sale;
                     }
-                    loading("Harvest");
+                    loading("Harvest", 2);
+                }
+                else if (strcmp(f[num].status, "insect") == 0 && f[num].insect){
+                    printf("This plant have insect\n");
+                    loading("Loading", 2);
                 }
                 else {
                     printf("This plant not bloom\n");
-                    loading("Loading");
+                    loading("Loading", 2);
                 }
             }
             else if (f[num].use == 0){
                 printf("This farm don't have plant.\n");
-                loading("Loading");
+                loading("Loading", 2);
             }
             else {
                 printf("Error\n");
@@ -430,12 +593,12 @@ void farm(){
         } 
         else if(istype("i") || istype("insecticides")){
             if(stamina <= 0){
-                loading("You don't have enough stamina.");
+                loading("You don't have enough stamina.", 2);
                 system("cls");
                 continue;
             }
             if(item[10].ea <= 0){
-                loading("You don't have insecticides");
+                loading("You don't have insecticides", 2);
                 system("cls");
                 continue;
             }
@@ -460,11 +623,11 @@ void farm(){
                 else strcpy(f[num].status, "usual");
                 printf("Stamina -1\n");
                 printf("Succeed\n");
-                loading("Use insecticides");
+                loading("Use insecticides", 2);
             }
             else if (f[num].use == 0){
                 printf("Don't have plant to use insecticides in this farm\n");
-                loading("Loading");
+                loading("Loading", 2);
             }
             else {
                 printf("Error\n");
@@ -474,12 +637,12 @@ void farm(){
         }
         else if(istype("t") || istype("stimulus")){
             if(item[12].ea <= 0){
-                loading("You don't have stimulus");
+                loading("You don't have stimulus", 2);
                 system("cls");
                 continue;
             }
             else{
-                loading("Use stimulus");
+                loading("Use stimulus", 2);
                 stamina += 2;
                 system("cls");
                 continue;
@@ -490,7 +653,7 @@ void farm(){
             break;
         }
         else{
-            loading("Error");
+            loading("Error", 2);
         }
         system("cls");
     }
@@ -549,7 +712,7 @@ int main(){
             printf(" type [X] to exit this game\n");
             printf("\n");
             printf("******************************************************\n");
-            printf("event:\n");
+            printf("event: %s\n", event);
             printf("******************************************************\n");
             if (error){
                 printf("ERROR\n");
@@ -562,6 +725,10 @@ int main(){
                 home();
             }
             else if (istype("s") || istype("shop")){
+                if (shop_close){
+                    loading("Shop_close", 5);
+                    continue;
+                }
                 shop();
             }
             else if (istype("f") || istype("farm")){
